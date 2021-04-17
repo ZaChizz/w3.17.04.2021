87ee6e41e61c8e7dcc5ecf76d7046ee7e7408566
caiman <me@caiman.dev>
2021-04-17 18:00:08 +0300

    [master] Merge sort

--- templates/merge.c
+++ templates/merge.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 
+#define LEN 10
+
 void merge(int array[], int lo, int mid, int hi) {
-    int tmp[hi-lo];
+    int buffer[hi-lo];
+    int i = lo;
+    int j = mid;
+    int k = 0;
 
-    // code should be here
+    for ( ; i < mid && j < hi; k++ ) {
+        if ( array[i] < array[j] ) {
+            buffer[k] = array[i];
+            i += 1;
+        } else {
+            buffer[k] = array[j];
+            j += 1;
+        }
+    }
+    for ( ; i < mid; i++, k++ ) {
+        buffer[k] = array[i];
+    }
+    for ( ; j < hi; j++, k++ ) {
+        buffer[k] = array[j];
+    }
 
-    for ( int i = 0, j = lo; j < hi; i++, j++ ) {
-        array[j] = tmp[i];
+    for ( int i = lo, k = 0; i < hi; i++, k++ ) {
+        array[i] = buffer[k];
     }
 }
 
@@ -19,11 +38,27 @@ void arrayPrint(int array[], int size) {
     printf("%d\n", array[last]);
 }
 
-int main() {
-    int array[10] = {1,2,3,4,5,3,4,5,6,7};
+void mergeSort(int array[], int start, int end) {
+    int middle = (start + end) / 2;
 
-    merge(array, 0, 5, 10);
-    arrayPrint(array, 10);
+    if ( start < middle ) {
+        printf("RECURSION CALL => start:%d middle:%d end:%d\n", start, middle, end);
+        mergeSort(array, start, middle);
+        mergeSort(array, middle, end);
+        printf("MERGE CALL => start:%d middle:%d end:%d\n", start, middle, end);
+        merge(array, start, middle, end);
+    }
+}
+
+
+int main() {
+    int array[LEN] = {10, 3, 7, 1, 6, 9, 2, 4, 8, 5};
+
+    arrayPrint(array, LEN);
+
+    mergeSort(array, 0, LEN);
+
+    arrayPrint(array, LEN);
 
     return 0;
 }
