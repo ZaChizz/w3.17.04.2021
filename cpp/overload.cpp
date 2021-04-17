5742fc43005711f339ccb21f098b2f3d8266c468
Ievgen <promptsite.com.ua@gmail.com>
2021-04-17 23:53:40 +0300

    Update overload.cpp
    Add float implement

--- cpp/overload.cpp
+++ cpp/overload.cpp
@@ -4,6 +4,10 @@ int increment(int value) {
     return value + 1;
 }
 
+float increment(float value) {
+    return value + 5;
+}
+
 char increment(char value) {
     return value + 1;
 }
@@ -19,7 +23,7 @@ long increment(long value) {
 int main() {
     std::cout << increment(10) << std::endl;
     std::cout << increment('A') << std::endl;
-    std::cout << increment(10.4) << std::endl;
+    std::cout << increment((float)10.4) << std::endl;
     std::cout << increment(10L) << std::endl;
     return 0;
 }
