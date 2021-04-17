28a9b1a5b4d1969bcbd9c665960f0813e4c86d17
caiman <me@caiman.dev>
2021-04-17 17:27:08 +0300

    [master] Quick sort

--- templates/partition.c
+++ templates/partition.c
@@ -3,7 +3,28 @@
 #define LEN 10
 
 int partition(int array[], int start, int end) {
-    // your code should be here
+    int tmp = (start + end) / 2;
+    int target = array[tmp];
+    int tail = start;
+
+    array[tmp] = array[end];
+    array[end] = target;
+
+    for ( ; array[tail] < array[end]; tail++ );
+    for ( int i = tail + 1; i < end; i++ ) {
+        if ( array[i] < array[end] ) {
+            tmp = array[i];
+            array[i] = array[tail];
+            array[tail] = tmp;
+            tail += 1;
+        }
+    }
+
+    tmp = array[end];
+    array[end] = array[tail];
+    array[tail] = tmp;
+
+    return tail;
 }
 
 void arrayPrint(int array[], int size) {
@@ -15,16 +36,25 @@ void arrayPrint(int array[], int size) {
     printf("%d\n", array[last]);
 }
 
+void quickSort(int array[], int start, int end) {
+    int index;
+
+    if ( end - start > 0 ) {
+        index = partition(array, start, end);
+
+        quickSort(array, start, index-1);
+        quickSort(array, index+1, end);
+    }
+}
+
 int main() {
     int array[LEN] = {10, 3, 7, 1, 6, 9, 2, 4, 8, 5};
-    int index = partition(array, 0, LEN-1);
 
-    printf("index: %d\n", index);
+    arrayPrint(array, LEN);
+
+    quickSort(array, 0, LEN-1);
+
     arrayPrint(array, LEN);
 
     return 0;
 }
-
-// should be:
-// index: 5
-//
